b1a0483aa27d07698f58a39f00e415f266029827
Ahmed Borwin <56191075+Ahmedborwin@users.noreply.github.com>
2024-03-07 11:27:45 +0000

    personal page Ahmed

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -22,10 +22,24 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("BatchRegistry", {
+  // await deploy("BatchRegistry", {
+  //   from: deployer,
+  //   // Contract constructor arguments
+  //   args: [deployer],
+  //   log: true,
+  //   // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+  //   // automatically mining the contract deployment transaction. There is no effect on live networks.
+  //   autoMine: true,
+  // });
+
+  // // Get the deployed contract to interact with it after deploying.
+  // const batchRegistry = await hre.ethers.getContract<Contract>("BatchRegistry", deployer);
+  // console.log("BatchRegistry deployed to:", await batchRegistry.getAddress());
+
+  await deploy("CheckIn", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: ["0x9386d925B1e18D231565222Bc14277f8B7f7FcF5"],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
@@ -33,8 +47,8 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const batchRegistry = await hre.ethers.getContract<Contract>("BatchRegistry", deployer);
-  console.log("BatchRegistry deployed to:", await batchRegistry.getAddress());
+  const CheckIn = await hre.ethers.getContract<Contract>("CheckIn", deployer);
+  console.log("CheckIn deployed to:", await CheckIn.getAddress());
 };
 
 export default deployYourContract;
---
+++ packages/nextjs/app/builders/0x5f2AF68dF96F3e58e1a243F4f83aD4f5D0Ca6029/page.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import type { NextPage } from "next";
+
+const AhmedBIO: NextPage = () => {
+  return (
+    <div className=" container mx-auto px-4 py-8">
+      <div className="UserProfile">
+        <h1 className="text-2xl font-bold text-center">Ahmed Borwin</h1>
+        <Image src="/HourGlass.png" alt="Avatar" width={200} height={200} />
+      </div>
+
+      <div className="my-8">
+        <h2 text-xl font-semibold>
+          Bio
+        </h2>
+        <p className="text-base">
+          Born in Libya, north Africa, raised in the united Kingdom Manchester. Graduated from Economics. Started
+          learning how to code 18 months ago. Submitted a project to the constellation hackathon called Chainrunners
+          which received a small prize. Also submitted zkCV to the recent circuit breaker hackathon. So much to learn. I
+          struggle with typescripts which makes it hard to contribute to open source projects. The goal is to build
+          meaningful user focused projects that will add value and or entertain. Long term goal is to study token
+          engineering. I find the idea of creating an economy very exciting!!
+        </p>
+      </div>
+
+      <div className="my-8">
+        <h2 text-xl font-semibold>
+          Socials
+        </h2>
+        <div className=" mt-4 grid grid-cols-1 sm:grid-cols-3 gap-4 text-center">
+          <a
+            href="https://github.com/AhmedBorwin"
+            className="text-blue-600 hover:text-blue-800"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://linkedin.com/in/ahmedborwindev"
+            className="text-blue-600 hover:text-blue-800"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://t.me/lostdecades"
+            className="text-blue-600 hover:text-blue-800"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Telegram
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AhmedBIO;
