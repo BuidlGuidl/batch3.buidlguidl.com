14e7b3099ee090363e10ee8f4b900be1bf506a34
Ahmed Borwin <56191075+Ahmedborwin@users.noreply.github.com>
2024-03-09 19:24:55 +0000

    remove unwanted file changes

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -22,24 +22,10 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  // await deploy("BatchRegistry", {
-  //   from: deployer,
-  //   // Contract constructor arguments
-  //   args: [deployer],
-  //   log: true,
-  //   // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-  //   // automatically mining the contract deployment transaction. There is no effect on live networks.
-  //   autoMine: true,
-  // });
-
-  // // Get the deployed contract to interact with it after deploying.
-  // const batchRegistry = await hre.ethers.getContract<Contract>("BatchRegistry", deployer);
-  // console.log("BatchRegistry deployed to:", await batchRegistry.getAddress());
-
-  await deploy("CheckIn", {
+  await deploy("BatchRegistry", {
     from: deployer,
     // Contract constructor arguments
-    args: ["0x9386d925B1e18D231565222Bc14277f8B7f7FcF5"],
+    args: [deployer],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
@@ -47,8 +33,8 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const CheckIn = await hre.ethers.getContract<Contract>("CheckIn", deployer);
-  console.log("CheckIn deployed to:", await CheckIn.getAddress());
+  const batchRegistry = await hre.ethers.getContract<Contract>("BatchRegistry", deployer);
+  console.log("BatchRegistry deployed to:", await batchRegistry.getAddress());
 };
 
 export default deployYourContract;
